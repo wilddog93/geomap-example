9dae03454799155692cc2fb14a8716c3e070776d
Jumakri Ridho Fauzi <RidhoInfinitec@INFINITECs-MacBook-Pro.local>
2023-12-05 12:02:02 +0700

    [ridho] - first installing next ui - wip

---
+++ app/new-page/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function NewPage() {
+	return (
+		<div>
+			<h1 className={title()}>New Page</h1>
+		</div>
+	);
+}
--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,53 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
+  name: "Konservasi Alam Nusantara",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Dashboard",
+      href: "/",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
+      label: "Table",
+      href: "/table",
     },
     {
-      label: "Blog",
-      href: "/blog",
+      label: "Reports",
+      href: "/reports",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
     },
     {
-      label: "About",
-      href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
-	},
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Table",
+      href: "/projects",
+    },
+    {
+      label: "Reports",
+      href: "/reports",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
 };
