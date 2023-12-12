2b0406409ea0956b18b6cb47902a6a748e607038
Jumakri Ridho Fauzi <RidhoInfinitec@INFINITECs-MacBook-Pro.local>
2023-12-12 11:57:17 +0700

    refactor sidebar map to hide map - wip

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
             sidebar ? "translate-x-0 lg:w-1/2" : "-translate-x-full w-0"
           }`}
         >
-          <ScrollShadow hideScrollBar className="w-full h-full">
+          <ScrollShadow hideScrollBar className="w-full h-full max-h-[700px]">
             <button
               type="button"
               className="inline-flex lg:hidden absolute z-10 right-1 mt-1 p-1 rounded-sm bg-white shadow"
@@ -59,101 +59,111 @@ export default function Home() {
         </div>
 
         <div
-          className={`relative w-full p-4 border-l-2 border-stroke ${sidebar ? "lg:w-1/2" : ""}`}
+          className={`relative w-full p-4 border-l-2 border-stroke ${
+            sidebar ? "lg:w-1/2" : ""
+          }`}
         >
-          <div className="relative">
-            <button
-              type="button"
-              className={`fixed lg:absolute z-10 rounded-l-lg px-1 py-2 bg-white shadow rounded-r-none -left-[2.5rem] group group-hover:bg-white 
-              ${!sidebar ? "left-5 lg:left-0 top-20 lg:top-0 rounded-r-lg" : "top-6"}
+          <button
+            type="button"
+            className={`fixed lg:absolute z-10 rounded-l-lg px-1 py-2 bg-white shadow rounded-r-none group group-hover:bg-white 
+              ${
+                !sidebar
+                  ? "left-5 top-20 lg:top-5 rounded-r-lg"
+                  : "-left-[1.6rem] top-6"
+              }
               `}
-              onClick={sideFunction}
+            onClick={sideFunction}
+          >
+            {!sidebar ? (
+              <div className="w-full max-w-max flex items-center gap-1">
+                <span className="duration-300 text-sm text-default-500">
+                  Map
+                </span>
+                <MdChevronRight className="w-4 h-4" />
+              </div>
+            ) : (
+              <MdChevronLeft className="w-4 h-4" />
+            )}
+          </button>
+          <ScrollShadow hideScrollBar className="w-full h-full max-h-[700px]">
+            <div
+              className={`w-full flex items-center px-4 mb-5 ${
+                !sidebar ? "mt-10" : "mt-5"
+              }`}
             >
-              {!sidebar ? (
-                <div className="w-full max-w-max flex items-center gap-1">
-                  <span className="duration-300 text-sm text-default-500">Map</span>
-                  <MdChevronRight className="w-4 h-4" />
-                </div>
-              ) : (
-                <MdChevronLeft className="w-4 h-4" />
-              )}
-            </button>
-            <ScrollShadow hideScrollBar className="w-full h-full">
-              <div className={`w-full flex items-center px-4 mb-5 ${!sidebar ? "mt-10" : "mt-5" }`}>
-                <div className="w-full flex flex-col gap-3 lg:1/2">
-                  <h3 className="font-bold text-xl">
-                    {items?.locationName || ""}
-                  </h3>
-                  <ul className="list-disc text-sm">
-                    {items?.description || items?.description?.length > 0
-                      ? items?.description?.map((desc: any) => {
-                          return <li key={desc}>{desc}</li>;
-                        })
-                      : null}
-                  </ul>
-                </div>
-                <div className="w-full lg:w-1/2">
-                  <Select
-                    radius="full"
-                    label=""
-                    className="w-full shadow-sm rounded-full bg-white dark:bg-default/60 backdrop-blur-xl backdrop-saturate-200 hover:bg-default-200/70 dark:hover:bg-default/70 group-data-[focused=true]:bg-default-200/50 dark:group-data-[focused=true]:bg-default/60"
-                    labelPlacement="outside"
-                    variant="bordered"
-                    listboxProps={{
-                      itemClasses: {
-                        base: [
-                          "text-default-500",
-                          "transition-opacity",
-                          "data-[hover=true]:text-foreground",
-                          "data-[hover=true]:bg-default-100",
-                          "dark:data-[hover=true]:bg-default-50",
-                          "data-[selectable=true]:focus:bg-default-50",
-                          "data-[pressed=true]:opacity-70",
-                          "data-[focus-visible=true]:ring-default-500",
-                        ],
-                      },
-                    }}
-                    color="primary"
-                    selectedKeys={[isSelected]}
-                    onChange={handleSelectionChange}
-                  >
-                    {dataSelects.map((data) => (
-                      <SelectItem key={data.value} value={data.value}>
-                        {data.label}
-                      </SelectItem>
-                    ))}
-                  </Select>
-                </div>
+              <div className="w-full flex flex-col gap-3 lg:1/2">
+                <h3 className="font-bold text-xl">
+                  {items?.locationName || ""}
+                </h3>
+                <ul className="list-disc text-sm">
+                  {items?.description || items?.description?.length > 0
+                    ? items?.description?.map((desc: any) => {
+                        return <li key={desc}>{desc}</li>;
+                      })
+                    : null}
+                </ul>
               </div>
-              <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-                <div className="w-full flex flex-col">
-                  <p className="text-xs mb-2">Parameter 1</p>
-                  <p className="font-bold text-lg">55.92</p>
-                  <p className="text-xs">Condition/status</p>
-                </div>
-
-                <div className="w-full flex flex-col">
-                  <p className="text-xs mb-2">Parameter 2</p>
-                  <p className="font-bold text-lg">55.92</p>
-                  <p className="text-xs">Condition/status</p>
-                </div>
-
-                <div className="w-full flex flex-col">
-                  <p className="text-xs mb-2">Parameter 3</p>
-                  <p className="font-bold text-lg">55.92</p>
-                  <p className="text-xs">Condition/status</p>
-                </div>
-
-                <div className="w-full flex flex-col">
-                  <p className="text-xs mb-2">Parameter 4</p>
-                  <p className="font-bold text-lg">55.92</p>
-                  <p className="text-xs">Condition/status</p>
-                </div>
+              <div className="w-full lg:w-1/2">
+                <Select
+                  radius="full"
+                  label=""
+                  className="w-full shadow-sm rounded-full bg-white dark:bg-default/60 backdrop-blur-xl backdrop-saturate-200 hover:bg-default-200/70 dark:hover:bg-default/70 group-data-[focused=true]:bg-default-200/50 dark:group-data-[focused=true]:bg-default/60"
+                  labelPlacement="outside"
+                  variant="bordered"
+                  listboxProps={{
+                    itemClasses: {
+                      base: [
+                        "text-default-500",
+                        "transition-opacity",
+                        "data-[hover=true]:text-foreground",
+                        "data-[hover=true]:bg-default-100",
+                        "dark:data-[hover=true]:bg-default-50",
+                        "data-[selectable=true]:focus:bg-default-50",
+                        "data-[pressed=true]:opacity-70",
+                        "data-[focus-visible=true]:ring-default-500",
+                      ],
+                    },
+                  }}
+                  color="primary"
+                  selectedKeys={[isSelected]}
+                  onChange={handleSelectionChange}
+                >
+                  {dataSelects.map((data) => (
+                    <SelectItem key={data.value} value={data.value}>
+                      {data.label}
+                    </SelectItem>
+                  ))}
+                </Select>
+              </div>
+            </div>
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+              <div className="w-full flex flex-col">
+                <p className="text-xs mb-2">Parameter 1</p>
+                <p className="font-bold text-lg">55.92</p>
+                <p className="text-xs">Condition/status</p>
               </div>
 
-              <ContentComponent />
-            </ScrollShadow>
-          </div>
+              <div className="w-full flex flex-col">
+                <p className="text-xs mb-2">Parameter 2</p>
+                <p className="font-bold text-lg">55.92</p>
+                <p className="text-xs">Condition/status</p>
+              </div>
+
+              <div className="w-full flex flex-col">
+                <p className="text-xs mb-2">Parameter 3</p>
+                <p className="font-bold text-lg">55.92</p>
+                <p className="text-xs">Condition/status</p>
+              </div>
+
+              <div className="w-full flex flex-col">
+                <p className="text-xs mb-2">Parameter 4</p>
+                <p className="font-bold text-lg">55.92</p>
+                <p className="text-xs">Condition/status</p>
+              </div>
+            </div>
+
+            <ContentComponent />
+          </ScrollShadow>
         </div>
       </section>
     </main>
