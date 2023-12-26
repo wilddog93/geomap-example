4f0dddc5d155524172bb911e55c8be7b136a64e9
Jumakri Ridho Fauzi <RidhoInfinitec@INFINITECs-MacBook-Pro.local>
2023-12-26 20:13:20 +0700

    delete middleware api

--- api/weather.api.ts
+++ api/weather.api.ts
@@ -41,3 +41,22 @@ export default function useWeatherApi() {
 
   return { fetch, data, meta, error, fetching };
 }
+
+
+// middleware.ts
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import {  useAuth } from '@/stores/auth';
+
+// export const isAuthenticated = (
+//   handler: (req: NextApiRequest, res: NextApiResponse) => void
+// ) => async (req: NextApiRequest, res: NextApiResponse) => {
+//   const { isAuth } = useAuth.getState();
+//   const token = req.cookies?.token
+
+//   if (!isAuth) {
+//     res.status(401).json({ error: 'Unauthorized' });
+//     return;
+//   }
+
+//   return handler(req, res);
+// };
